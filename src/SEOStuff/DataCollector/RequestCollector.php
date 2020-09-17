1a3e5ee28e8e9940aa3c2cfbf15c66c14c033227
Anne-Laure de Boissieu <al.deboissieu@elao.com>
2020-09-17 15:36:26 +0200

    wip

--- src/SEOStuff/DataCollector/RequestCollector.php
+++ src/SEOStuff/DataCollector/RequestCollector.php
@@ -11,8 +11,9 @@ class RequestCollector extends DataCollector
     public function collect(Request $request, Response $response, \Throwable $exception = null)
     {
         $this->data = [
-            'method' => $request->getMethod(),
+            'method' => $response,
             'acceptable_content_types' => $request->getAcceptableContentTypes(),
+            'X-Robots-Tag' => $response->headers->count()
         ];
     }
 
@@ -33,5 +34,11 @@ class RequestCollector extends DataCollector
     public function getAcceptableContentTypes()
     {
         return $this->data['acceptable_content_types'];
+    }
+    public function getXRobotsTag()
+    {
+        dump($this->data['X-Robots-Tag']);
+        die;
+        return $this->data['X-Robots-Tag'];
     }
 }

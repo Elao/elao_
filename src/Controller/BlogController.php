2698e8f99b3726ff6fdcc2ac1f5cbc69b342e11f
Thomas Jarrand <thomas.jarrand@elao.com>
2021-01-12 14:18:15 +0100

    Fixed page url format

--- src/Controller/BlogController.php
+++ src/Controller/BlogController.php
@@ -25,7 +25,7 @@ class BlogController extends AbstractController
 
     /**
      * @Route("/", name="blog")
-     * @Route("/page-{!page}", name="blog_page", requirements={"page"="\d+"})
+     * @Route("/page/{!page}", name="blog_page", requirements={"page"="\d+"})
      */
     public function index(int $page = 1, int $perPage = 20): Response
     {

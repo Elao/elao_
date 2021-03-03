f04cc9cf0bfe029f1020cbd33af3332fa00f7028
Maxime Steinhausser <maxime.steinhausser@elao.com>
2021-03-03 11:35:15 +0100

    Move back controllers.json to assets root
    so Flex can auto-provide the Symfony UX controllers when installed

--- webpack.config.js
+++ webpack.config.js
@@ -27,7 +27,7 @@ Encore
     .addStyleEntry('style', './assets/scss/style.scss')
 
     // enables the Symfony UX Stimulus bridge (used in assets/bootstrap.js)
-    .enableStimulusBridge('./assets/js/controllers.json')
+    .enableStimulusBridge('./assets/controllers.json')
 
     // When enabled, Webpack "splits" your files into smaller pieces for greater optimization.
     .splitEntryChunks()

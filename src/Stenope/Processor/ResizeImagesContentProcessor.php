7b4202d27d49b2e82c4ce8335ee80dcfcac7308f
Maxime Steinhausser <maxime.steinhausser@elao.com>
2021-03-25 15:43:04 +0100

    Resize images inside case studies & articles contents + support for retina screens

---
+++ src/Stenope/Processor/ResizeImagesContentProcessor.php
@@ -0,0 +1,86 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Stenope\Processor;
+
+use App\Bridge\Glide\Bundle\ResizedUrlGenerator;
+use Stenope\Bundle\Behaviour\ProcessorInterface;
+use Stenope\Bundle\Content;
+use Symfony\Component\DomCrawler\Crawler;
+
+/**
+ * Provide resized and optimized images, for retina devices as well, inside a specific content.
+ */
+class ResizeImagesContentProcessor implements ProcessorInterface
+{
+    private ResizedUrlGenerator $resizedUrlGenerator;
+
+    private string $type;
+    private string $preset;
+    private string $property;
+
+    public function __construct(
+        ResizedUrlGenerator $resizedUrlGenerator,
+        string $type,
+        string $preset,
+        string $property = 'content'
+    ) {
+        $this->resizedUrlGenerator = $resizedUrlGenerator;
+        $this->type = $type;
+        $this->preset = $preset;
+        $this->property = $property;
+    }
+
+    public function __invoke(array &$data, string $type, Content $content): void
+    {
+        if ($type !== $this->type) {
+            return;
+        }
+
+        $crawler = new Crawler($data[$this->property]);
+
+        try {
+            $crawler->html();
+        } catch (\Exception $e) {
+            // Content is not valid HTML.
+            return;
+        }
+
+        $crawler = new Crawler($data[$this->property]);
+
+        /** @var \DomElement $element * */
+        foreach ($crawler->filter('img') as $element) {
+            $this->processImage($element);
+        }
+
+        $data[$this->property] = $crawler->html();
+    }
+
+    private function processImage(\DOMElement $element): void
+    {
+        if (!$element->hasAttribute('src')) {
+            return;
+        }
+
+        $source = $element->getAttribute('src');
+
+        if (!$this->isLocalImage($source)) {
+            return;
+        }
+
+        $dpr1 = $this->resizedUrlGenerator->withPreset($source, $this->preset);
+        $dpr2 = $this->resizedUrlGenerator->withPreset($source, $this->preset, ['dpr' => 2]);
+
+        $element->setAttribute('src', $dpr1);
+        $element->setAttribute('srcset', <<<HTML
+        $dpr1 1x,
+        $dpr2 2x,
+        HTML);
+    }
+
+    private function isLocalImage(string $url): bool
+    {
+        return \is_string(parse_url($url, PHP_URL_HOST));
+    }
+}

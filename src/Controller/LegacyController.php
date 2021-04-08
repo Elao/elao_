cb9241a74349754ff8cc229192013c13aa14421c
Maxime Steinhausser <maxime.steinhausser@elao.com>
2021-04-08 15:52:25 +0200

    PHP 8

--- src/Controller/LegacyController.php
+++ src/Controller/LegacyController.php
@@ -11,9 +11,9 @@ use Symfony\Component\Routing\Annotation\Route;
 
 class LegacyController extends AbstractController
 {
-    /**
-     * @Route("/.htaccess", name="htaccess", format="htaccess", options={"stenope"={"ignore"=true}})
-     */
+    #[Route('/.htaccess', name: 'htaccess', options: [
+        'stenope' => ['ignore' => true],
+    ], format: 'htaccess')]
     public function htaccess(HtaccessGenerator $generator): Response
     {
         return new Response($generator->generateHtAccess(HtaccessGenerator::TARGET_SITE));

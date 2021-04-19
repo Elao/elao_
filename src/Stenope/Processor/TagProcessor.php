848f6e86810fb933f0b9899fe006b80eb47324ad
Thomas Jarrand <thomas.jarrand@elao.com>
2021-04-19 16:59:41 +0200

    Slugify tags (to avoid white spaces).

---
+++ src/Stenope/Processor/TagProcessor.php
@@ -0,0 +1,36 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Stenope\Processor;
+
+use App\Model\Article;
+use Stenope\Bundle\Behaviour\ContentManagerAwareInterface;
+use Stenope\Bundle\Behaviour\ContentManagerAwareTrait;
+use Stenope\Bundle\Behaviour\ProcessorInterface;
+use Stenope\Bundle\Content;
+use Symfony\Component\String\Slugger\SluggerInterface;
+
+/**
+ * Sanitize tag list
+ */
+class TagProcessor implements ProcessorInterface, ContentManagerAwareInterface
+{
+    use ContentManagerAwareTrait;
+
+    private SluggerInterface $slugger;
+
+    public function __construct(SluggerInterface $slugger)
+    {
+        $this->slugger = $slugger;
+    }
+
+    public function __invoke(array &$data, string $type, Content $content): void
+    {
+        if (!is_a($type, Article::class, true)) {
+            return;
+        }
+
+        $data['tags'] = array_map([$this->slugger, 'slug'], $data['tags']);
+    }
+}

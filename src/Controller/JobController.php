96eba1d33acc361891273ed8269ab5b9842ce001
amelie <amelie.defrance@elao.com>
2021-04-28 14:55:24 +0200

    Think i did something wrong

--- src/Controller/JobController.php
+++ src/Controller/JobController.php
@@ -11,6 +11,9 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
+/**
+ * @Route("/recrutement")
+ */
 class JobController extends AbstractController
 {
     private ContentManager $manager;

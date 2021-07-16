d1758d88bf5f53bc7057c64545c8266b7f8a13b8
Thomas Jarrand <thomas.jarrand@elao.com>
2021-07-16 10:50:02 +0200

    Introduce formats

--- assets/js/feature/SocialPostGenerator.js
+++ assets/js/feature/SocialPostGenerator.js
@@ -6,13 +6,23 @@ export default class SocialPostGenerator {
 
         this.loadTheme = this.loadTheme.bind(this);
         this.loadTemplate = this.loadTemplate.bind(this);
+        this.loadFormat = this.loadFormat.bind(this);
 
         this.form.children.theme.addEventListener('change', this.loadTheme);
         this.form.children.template.addEventListener('change', this.loadTemplate);
+        this.form.children.format.addEventListener('change', this.loadFormat);
+
+        this.loadTheme();
+        this.loadFormat();
+        this.loadTemplate();
     }
 
     loadTheme() {
         this.target.dataset.theme = this.form.children.theme.value;
+    }
+
+    loadFormat() {
+        this.target.dataset.format = this.form.children.format.value;
     }
 
     loadTemplate() {

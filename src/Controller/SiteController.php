a151f2539c940af4982f18c3dcf5c5ae75725cbd
amelie <amelie.defrance@elao.com>
2021-09-24 09:20:53 +0200

    Init page

--- src/Controller/SiteController.php
+++ src/Controller/SiteController.php
@@ -33,6 +33,12 @@ class SiteController extends AbstractController
         return $this->render('site/services.html.twig');
     }
 
+    #[Route('/conception-ux-ui', name: 'ux-ui-design')]
+    public function uxUiDesign(): Response
+    {
+        return $this->render('site/ux-ui-design.html.twig');
+    }
+
     #[Route('/methodologie', name: 'methodology')]
     public function methodology(): Response
     {

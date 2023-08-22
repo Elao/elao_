be16242a799dd08266962b1e3ab41881f6843525
Amélie <amelie.defrance@elao.com>
2023-08-22 09:05:46 +0100

    Update team page route

--- src/Controller/TeamController.php
+++ src/Controller/TeamController.php
@@ -13,7 +13,7 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
-#[Route('/la-tribu')]
+#[Route('/la-team')]
 class TeamController extends AbstractController
 {
     private ContentManagerInterface $manager;

fa3f0ce9ba2e52415b275cb39ee7eac4b42d49e4
Amélie <amelie.defrance@elao.com>
2023-08-30 09:59:38 +0100

    Update team wording

--- src/Controller/TeamController.php
+++ src/Controller/TeamController.php
@@ -13,7 +13,7 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
-#[Route('/la-team')]
+#[Route('/equipe')]
 class TeamController extends AbstractController
 {
     private ContentManagerInterface $manager;
